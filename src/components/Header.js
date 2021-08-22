7a506d055f2615c37e42e511d6311e1e6b0561ce
Jose Manuel <castellanogonzalezjosemanuel@gmail.com>
2021-08-22 13:31:33 +0200

    version 3

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,30 @@
 import '../styles/Header.css';
-export const Header = () => {
+import React from 'react';
+const Header = () => {
   return (
-    <div className="Application">
-      <div className="header">
-        <a id="logo-header" href="#Bio">
-          <span className="site-name">José Manuel Castellano González</span>
-          <span className="site-desc">Desarrollador software Junior</span>
-        </a>
-        <nav>
-          <ul>
-            <li>
-              <a href="/#">Inicio</a>
-            </li>
-            <li>
-              <a href="/#bio">Sobre mí</a>
-            </li>
-            <li>
-              <a href="/#contact">Contacto</a>
-            </li>
-          </ul>
-        </nav>
+    <header>
+      <div className="Application">
+        <div className="header">
+          <a id="logo-header" href="#Bio">
+            <span className="site-name">José Manuel Castellano González</span>
+            <span className="site-desc">Desarrollador software Junior</span>
+          </a>
+          <nav>
+            <ul>
+              <li>
+                <a href="/">Inicio</a>
+              </li>
+              <li>
+                <a href="/bio">Sobre mí</a>
+              </li>
+              <li>
+                <a href="/contact">Contacto</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
-    </div>
+    </header>
   );
 };
 export default Header;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,15 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from './App.js';
-import { Header } from './Header';
+import Header from './Header';
 import { Bio } from './Bio';
 import { Contact } from './Contact';
 
 export const Main = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         <Route exact path={'/'} component={App} />
-        <Route path={'/'} component={Header} />
         <Route path={'/bio'} component={Bio} />
         <Route path={'/contact'} component={Contact} />
       </Switch>
