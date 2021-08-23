42a5d608d4eb77512963e20f7e6865fce12bbb95
Jose Manuel <castellanogonzalezjosemanuel@gmail.com>
2021-08-23 23:11:41 +0200

    resuelto enlaces

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import '../styles/App.css';
 import '../styles/normalize.css';
+import crud from '../assets/CRUD2.png';
+import playa from '../assets/playa2.png';
 function App() {
   return (
     <body>
@@ -14,32 +16,44 @@ function App() {
         </div>
         <div className="text1">
           <div className="part1">
-            {<img src="../assets/CRUD2.png" alt="" />}
+            <a href="https://focused-mirzakhani-101c93.netlify.app">
+              <img src={crud} alt="" />
+            </a>
           </div>
+
           <div className="part2">
             <a href={'https://focused-mirzakhani-101c93.netlify.app'}>
-              <b>Aplicación CRUD en stack MERN.</b>
+              <b>
+                Aplicación CRUD(Create, Read, Update, Delete) en stack MERN.
+              </b>
             </a>
             <p />
             <br></br>
             <p>
               Las tecnologías usadas son React, MongoDb para la base de datos,
-              Express y NodeJs para el servidor.
+              Express y NodeJs para el servidor. Su funcionamiento es rellenando
+              dos campos.
             </p>
+            <br></br>
           </div>
         </div>
         <div className="mainText2"></div>
         <div className="text2">
-          <div className="part3"></div>
+          <div className="part3">
+            <a href="https://sleepy-northcutt-f36bee.netlify.app">
+              <img src={playa} alt="" />
+            </a>
+          </div>
           <div className="part4">
             <a href={'https://sleepy-northcutt-f36bee.netlify.app'}>
-              <b>Aplicación buscador en React</b>
+              <b>Buscador en React</b>
             </a>
             <p />
             <br></br>
             <p>
               Buscador de playas de España hecho en React y que consume una Api
-              Rest pública.
+              Rest pública. Introduzca el nombre de una ciudad costera para ver
+              sus playas
             </p>
           </div>
         </div>
