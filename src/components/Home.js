0d96472071c26cf8795b885b328a9c7c1a35494d
Jose Manuel <castellanogonzalezjosemanuel@gmail.com>
2021-12-12 18:34:40 +0100

    actualizacion

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,26 @@
 import { useHistory } from 'react-router-dom';
-import playa from '../assets/playa2.png';
+import playa from '../assets/Playas.png';
 import home from '../assets/home.svg';
 import email from '../assets/email.svg';
 import crud from '../assets/CRUD2.png';
 import perfil from '../assets/Perfil.png';
 import snake from '../assets/Snake.png';
-import notas from '../assets/Notas.png';
+import Restaurante from '../assets/Restaurante.png';
 import '../style/Home.css';
 import '../style/normalize.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  Card,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardText,
+  Button,
+  Container,
+  Row,
+  Col,
+} from 'reactstrap';
 
 const Home = () => {
   const history = useHistory();
@@ -53,8 +66,93 @@ const Home = () => {
             <h2>José Manuel Castellano Gonzalez</h2>
             <h2>Desarrollador software Junior</h2>
           </div>
-          <div className="imagenes">
-            <div className="cuadro">
+
+          {/* reactstrap */}
+
+          <Container className="col col-12">
+            <Row>
+              <Col className="col-4 offset-1 mt-5">
+                <Card>
+                  <CardImg alt="Card image cap" src={playa} top width="100%" />
+                  <CardBody>
+                    <CardTitle tag="h5">Buscador de playas</CardTitle>
+                    <CardSubtitle className="mb-2 text-muted" tag="h6">
+                      React
+                    </CardSubtitle>
+                    <CardText>
+                      Buscador de playas de España. Introduzca el nombre de una
+                      ciudad costera para ver sus playas y sus detalles.
+                    </CardText>
+                    <Button href="https://sleepy-northcutt-f36bee.netlify.app">
+                      Visitar buscador
+                    </Button>
+                  </CardBody>
+                </Card>
+              </Col>
+
+              <Col className="col-4 offset-3 mt-5">
+                <Card>
+                  <CardImg alt="Card image cap" src={crud} top width="100%" />
+                  <CardBody>
+                    <CardTitle tag="h5">C.R.U.D.</CardTitle>
+                    <CardSubtitle className="mb-2 text-muted" tag="h6">
+                      React, Node, Express, MongoDb
+                    </CardSubtitle>
+                    <CardText>
+                      Aplicación CRUD que funciona introduciento el nombre y la
+                      edad de una persona para poder crearla, editarla o
+                      borrarla de la base de datos.
+                    </CardText>
+                    <Button href="https://focused-mirzakhani-101c93.netlify.app">
+                      Ejecutar aplicación
+                    </Button>
+                  </CardBody>
+                </Card>
+              </Col>
+
+              <Col className="col-4 offset-1 mt-5">
+                <Card>
+                  <CardImg
+                    alt="Card image cap"
+                    src={Restaurante}
+                    top
+                    width="100%"
+                  />
+                  <CardBody>
+                    <CardTitle tag="h5">Web restaurante</CardTitle>
+                    <CardSubtitle className="mb-2 text-muted" tag="h6">
+                      HTML, CSS, Javascript, Bootstrap.
+                    </CardSubtitle>
+                    <CardText>
+                      Web para la sección de comida a domicilio de un
+                      restaurante.
+                    </CardText>
+                    <Button href="https://zealous-newton-1adf4f.netlify.app">
+                      Visitar web
+                    </Button>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col className="col-4 offset-3 mt-5">
+                <Card>
+                  <CardImg alt="Card image cap" src={snake} top width="100%" />
+                  <CardBody>
+                    <CardTitle tag="h5">Snake</CardTitle>
+                    <CardSubtitle className="mb-2 text-muted" tag="h6">
+                      Vanilla Javascript
+                    </CardSubtitle>
+                    <CardText>
+                      Para terminar un juego, el clasico Snake.
+                    </CardText>
+                    <Button href="https://snake-4c9ffb.netlify.app/">
+                      Jugar
+                    </Button>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+          {/* <div className="cuadro">
               <div className="img1">
                 <a href="https://sleepy-northcutt-f36bee.netlify.app">
                   <img
@@ -114,8 +212,7 @@ const Home = () => {
 
                 <p>Clásico juego de la serpiente (vanilla Javascript).</p>
               </div>
-            </div>
-          </div>
+            </div> */}
         </div>
       </div>
     </div>
--- src/components/Info.js
+++ src/components/Info.js
@@ -49,17 +49,17 @@ const Info = () => {
       <div className="descripcion">
         <p className="name">Jose Manuel Castellano Gonzalez</p>
         <p className="bio">
-          Soy un chico de Algeciras al que le gusta la informática y la
+          Soy un chico de Algeciras al que le apasiona la informática y la
           programación. He vivido la informática desde la llegada de los
           ordenadores personales y desde entonces he aprendido a programar y ha
           sido mi pasión. Mis estudios son de Ingeniería de Telecomunicaciones
           sin terminar, pero la parte software y de programación la tengo
           terminada. Realicé un bootcamp de Sofware Engineering en General
-          Assembly para lo que me preparé para el mundo laboral de una forma
-          buena. Mis conocimientos para el desarrollo web son amplios aunque
+          Assembly para lo que me preparé para el mundo laboral de una buena
+          forma. Mis conocimientos para el desarrollo web son amplios aunque
           sigo practicando y mejorando todos los dias. Las tecnologías que
-          practico son entre otras Javascipt, CSS, HTML, React, Node, MongoDB y
-          Express.
+          practico son entre otras Javascipt, CSS, HTML, React, Node, MongoDB,
+          Express y Bootstrap.
         </p>
       </div>
     </div>
